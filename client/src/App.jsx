4f3c14c25c02b8695236404048e688b948286ef9
St Pardon <okc4pardon@gmail.com>
2023-03-11 19:18:52 +0100

    :hammer: added chat component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,33 @@ import LandingPage from './routes/landing-page/LandingPage.route';
 import Signin from './auth/signin/Signin.auth';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Signup from './auth/signup/Signup.auth';
+import Chat from './routes/chat/chat.route';
+import Messages from './routes/messages/Messages.route';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
   const router = createBrowserRouter([
     { path: '/', element: <LandingPage /> },
     { path: '/signin', element: <Signin /> },
     { path: '/signup', element: <Signup /> },
+    {
+      path: '/chat',
+      element: <Chat />,
+      children: [
+        {
+          path: ':user',
+          element: <Messages />,
+        },
+      ],
+    },
   ]);
-  return <RouterProvider router={router} />;
+
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -2,7 +2,12 @@ import express from 'express';
 import http from 'http';
 import { Server as Socket } from 'socket.io';
 import cors from 'cors';
+import { connectToMongoDB } from './config/db.config.js';
 import dotenv from 'dotenv';
+import { userModel } from './models/user.models.js';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import './middleware/auth.middleware.js';
 
 dotenv.config();
 
@@ -10,6 +15,7 @@ const PORT = process.env.PORT || 5000;
 const app = new express();
 const server = http.createServer(app);
 const io = new Socket(server);
+let chatId;
 
 app
   .use(cors())
@@ -19,26 +25,82 @@ app
   .get('/', (req, res) => {
     res.status(200).end('Kindly signin or signup');
   })
-  .post('signup', (req, res) => {
-    const { username, password, first_name, last_name, date_of_birth } =
-      req.body;
-    res.status(200).send('signup successful');
-  })
-  .post('signin', (req, res) => {
-    const { username, password } = req.body;
-    res.status(200).send('signup successful');
+  .post(
+    '/signup',
+    async (req, res, next) => {
+    passport.authenticate('signup', async (err, user)=>{
+      if (err){
+        console.log(err)
+        res.status(403).send(err)
+      }
+      if (!user){
+        const error = new Error('Username or password is incorrect');
+        console.log(error)
+        res.status(403).send("User already exist")
+      }
+    })(req, res, next)
+      // console.log("after middleware")
+      // res.status(201).json({
+      //   message: 'Signup successful',
+      //   user: req.user,
+      // });
+    }
+  )
+  .post('/signin', async (req, res, next) => {
+    passport.authenticate('signin', async (err, user) => {
+      try {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          const error = new Error('Username or password is incorrect');
+          return next(error);
+        }
+        req.login(user, { session: false }, async (error) => {
+          if (error) return next(error);
+          const body = { _id: user._id, email: user.email };
+          const token = jwt.sign({ user: body }, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+          });
+          return res.status(200).json({ message: 'Signin successful', token });
+        });
+      } catch (error) {
+        return next(error);
+      }
+    })(req, res, next);
   })
   .post('newchat', (req, res) => {})
   .get('chatlist', (req, res) => {})
   .get('chat:id', (req, res) => {})
   .post('chat:id', (req, res) => {});
 
-io.on('connection', (socket) => {
-  console.log('We have a new connection', socket.id);
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error('invalid username'));
+  }
+  socket.username = username;
+  next();
+});
 
+io.on('connection', (socket) => {
+  chatId = socket.id;
+  console.log('We have a new connection', socket.id);
+  console.log('We have a new connection', socket.username);
+
+  const users = [];
+  for (let [id, socket] of io.of('/').sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  socket.emit('users', users);
+
+  // console.log('We have a new connection', socket);
   socket.on('chat', (data) => {
     console.log(data);
-    
+
     if (data.socketid) {
       io.to(data.socketid).to(socket.id).emit('chat', data);
     } else {
@@ -51,6 +113,9 @@ io.on('connection', (socket) => {
   });
 });
 
+connectToMongoDB();
 server.listen(PORT, () => {
-  console.log(`server is running on http://127.0.0.1:${PORT}`);
+  console.log(
+    `server is up and running \nVisit http://127.0.0.1:${PORT} \nwaiting for database to connect...`
+  );
 });
