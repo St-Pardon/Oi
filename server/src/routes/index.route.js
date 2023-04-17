c31e5849b7eeba3d0736c453dc4f5a6454b0b783
St Pardon <okc4pardon@gmail.com>
2023-04-17 10:32:09 +0100

    :hammer: updated index route for group chat

--- server/src/routes/index.route.js
+++ server/src/routes/index.route.js
@@ -3,6 +3,7 @@ import AuthRoute from './auth.routes.js';
 import UserRoute from './user.routes.js';
 import ChatlistRoute from './chatlist.routes.js';
 import ChatRequestRouter from './chat-request.routes.js';
+import GroupRoute from './group.routes.js';
 
 const v1 = '/api/v1/';
 const indexRoute = Router();
@@ -12,6 +13,7 @@ indexRoute
   .use(`${v1}user/`, UserRoute)
   .use(`${v1}chatlist/`, ChatlistRoute)
   .use(`${v1}request`, ChatRequestRouter)
+  .use(`${v1}group`, GroupRoute)
   .get('/', (req, res) => {
     res.status(200).end('Kindly refer to the API documentation to et started');
   });
