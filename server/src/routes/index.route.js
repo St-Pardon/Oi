9aa3c513af265baa5bf9b025d24cc6b62e3ff5d8
St Pardon <okc4pardon@gmail.com>
2023-04-27 17:51:25 +0100

    :hammer: included authorization for protected routes

--- server/src/routes/index.route.js
+++ server/src/routes/index.route.js
@@ -5,21 +5,37 @@ import UserRoute from './user.routes.js';
 import ChatlistRoute from './chatlist.routes.js';
 import ChatRequestRouter from './chat-request.routes.js';
 import GroupRoute from './group.routes.js';
+import passport from 'passport';
 // import swaggerDocument from '../../swagger.json' assert { type: 'json' }
-
 
 const v1 = '/api/v1/';
 const indexRoute = Router();
 
 indexRoute
   .use(`${v1}auth`, AuthRoute)
-  .use(`${v1}user/`, UserRoute)
-  .use(`${v1}chatlist/`, ChatlistRoute)
-  .use(`${v1}request`, ChatRequestRouter)
-  .use(`${v1}group`, GroupRoute)
+  .use(
+    `${v1}user/`,
+    passport.authenticate('jwt', { session: false }),
+    UserRoute
+  )
+  .use(
+    `${v1}chatlist/`,
+    passport.authenticate('jwt', { session: false }),
+    ChatlistRoute
+  )
+  .use(
+    `${v1}request`,
+    passport.authenticate('jwt', { session: false }),
+    ChatRequestRouter
+  )
+  .use(
+    `${v1}group`,
+    passport.authenticate('jwt', { session: false }),
+    GroupRoute
+  )
   // .use(
   //   '/api-docs',
-  //   swaggerUi.serve, 
+  //   swaggerUi.serve,
   //   swaggerUi.setup(swaggerDocument)
   // )
   .get('/', (req, res) => {
