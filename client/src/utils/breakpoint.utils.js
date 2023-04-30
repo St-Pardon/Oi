0d82c43195acbae07e32e9d37e58060fdd51d5e3
St Pardon <okc4pardon@gmail.com>
2023-04-30 14:07:06 +0100

    :hammer: updated breakpoints

---
+++ client/src/utils/breakpoint.utils.js
@@ -0,0 +1,17 @@
+const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+};
+
+export const devices = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+};
